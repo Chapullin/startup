ac11d147a95e965ae0bd282592ae435633d9942f
Chapullin <molinero.cristian.d@gmail.com>
2015-10-01 16:11:10 -0300

    test 2 repeat problem

---
+++ 05-angularjs/test2/js/movies.js
@@ -0,0 +1,13 @@
+/*var movies = 
+	{
+		name: 'Terminator',
+		description: 'Los Ángeles, año 2029. Las máquinas dominan el mundo. Los rebeldes que luchan contra ellas tienen como líder a John Connor, un hombre que nació en los años ochenta. Para acabar con la rebelión, las máquinas deciden enviar al pasado a un robot -Terminator- cuya misión será eliminar a Sarah Connor, la madre de John, e impedir así su nacimiento. (FILMAFFINITY)',
+		shortName: 'terminator',
+		year: '1984',
+		duration: '108 min',
+		country: 'Estados Unidos',
+		director: 'James Cameron',
+		imageURL: 'terminator.jpg',
+		guion: 'James Cameron, Gale Anne Hurd'
+	}; */
+ 
