f8d7f840fee55765cfce42dd3ad152b2ffcabf90
Chapullin <molinero.cristian.d@gmail.com>
2015-10-02 11:16:51 -0300

    movie don't define

--- 05-angularjs/test2/js/app.js
+++ 05-angularjs/test2/js/app.js
@@ -1,74 +1,27 @@
-var movies = [
-	{
-		name: 'Terminator',
-		description: 'Los Ángeles, año 2029. Las máquinas dominan el mundo. Los rebeldes que luchan contra ellas tienen como líder a John Connor, un hombre que nació en los años ochenta. Para acabar con la rebelión, las máquinas deciden enviar al pasado a un robot -Terminator- cuya misión será eliminar a Sarah Connor, la madre de John, e impedir así su nacimiento. (FILMAFFINITY)',
-		shortName: 'terminator',
-		year: '1984',
-		duration: '108 min',
-		country: 'Estados Unidos',
-		director: 'James Cameron',
-		imageURL: 'terminator.jpg',
-		guion: 'James Cameron, Gale Anne Hurd'
-	},
-	{
-		name: 'Pulp Fiction',
-		description: 'Jules y Vincent, dos asesinos a sueldo con no demasiadas luces, trabajan para el gángster Marsellus Wallace.Vincent le confiesa a Jules que Marsellus le ha pedido que cuide de Mia, su atractiva mujer. Jules le recomienda prudencia porque es muy peligroso sobrepasarse con la novia del jefe. Cuando llega la hora de trabajar, ambos deben ponerse "manos a la obra". Su misión: recuperar un misterioso maletín. (FILMAFFINITY)',
-		shortName: 'pulp-fiction',
-		year: '1994',
-		duration: '153 min',
-		country: 'Estados Unidos',
-		director: 'Quentin Tarantino',
-		guion: 'Quentin Tarantino, Roger Avary',
-		imageURL: 'pulp-fiction.jpg'
-	},
-	{
-		name: 'Kill Bill',
-		description: 'El día de su boda, una asesina profesional (Thurman) sufre el ataque de algunos miembros de su propia banda, que obedecen las órdenes de Bill (David Carradine), el jefe de la organización criminal. Logra sobrevivir al ataque, aunque queda en coma. Cuatro años después despierta dominada por un gran deseo de venganza. (FILMAFFINITY)',
-		shortName: 'kill-bill',
-		year: '2003',
-		duration: '110 min',
-		country: 'Estados Unidos',
-		director: 'Quentin Tarantino',
-		guion: 'Quentin Tarantino',
-		imageURL: 'kill-bill.jpg'
-	},
-	{
-		name: 'Reservoir Dogs',
-		description: 'Una banda organizada es contratada para atracar una empresa y llevarse unos diamantes. Sin embargo, antes de que suene la alarma, la policía ya está allí. Algunos miembros de la banda mueren en el enfrentamiento con las fuerzas del orden, y los demás se reúnen en el lugar convenido. (FILMAFFINITY)',
-		shortName: 'reservoir-dogs',
-		year: '1992',
-		duration: '99 min',
-		country: 'Estados Unidos',
-		director: 'Quentin Tarantino',
-		guion: 'Quentin Tarantino',
-		imageURL: 'reservoir-dogs.jpg'
-	},
-	{
-		name: 'Jackass',
-		description: 'Las gamberradas de Johnny Knoxville y sus "colegas" en la popular MTV -todo un éxito de audiencia de la cadena televisiva especializada en música- es trasladada a la gran pantalla con una gran éxito de taquilla. (FILMAFFINITY)',
-		shortName: 'jackass',
-		year: '2002',
-		duration: '80 min',
-		country: 'Estados Unidos',
-		director: 'Jeff Tremaine',
-		guion: 'Johnny Knoxville, Spike Jonze, Jeff Tremaine',
-		imageURL: 'jackass.jpg'
-	},
-	{
-		name: 'Sin City',
-		description: 'En Sin City, ciudad de policías corruptos y atractivas mujeres, unos buscan venganza, otros, redención, o ambas cosas a la vez. Marv (Mickey Rourke) se propone vengar la muerte de su único amor. Dwight (Clive Owen) es un investigador privado con problemas que resolver. Hartigan (Bruce Willis), el único policía honrado de la ciudad, sigue la pista de una joven que está en manos del sádico hijo de un senador. (FILMAFFINITY)',
-		shortName: 'sin-city',
-		year: '2005',
-		duration: '124 min',
-		country: 'Estados Unidos',
-		director: 'Robert Rodriguez, Quentin Tarantino, Frank Miller',
-		guion: 'Frank Miller, Robert Rodriguez (Cómic: Frank Miller)',
-		imageURL: 'sin-city.jpg'
-	}
-];
 
-var app = angular.module('videoClub', [ ]);
+
+var app = angular.module('videoClub', ['ngRoute', 'moviescatControllers']);
 
 app.controller ('VideoclubController', function ($scope) {  
-	$scope.movie = movies[1];
+	$scope.movies = movies;
 });
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'app/partial/movie-list.html',
+        controller: 'MovieListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ 05-angularjs/test2/js/controller.js
@@ -0,0 +1,15 @@
+var moviescatControllers = angular.module('moviescatControllers', []);
+
+moviescatControllers.controller('MovieListCtrl', ['$scope', '$http',
+  function ($scope, $http) { //MovieListCtrl es el controlador dentro del route
+    $http.get('js/movies.json').success(function(data) {
+      $scope.movies = data;
+    });
+
+    $scope.orderProp = 'year';
+  }]);
+
+moviescatControllers.controller('MovieDetailCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    $scope.name = $routeParams.name;  //ex $scope.phoneId
+  }]);
